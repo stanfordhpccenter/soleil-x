ec496971c7ec10180400691ff262e40791243e21
Elliott Slaughter <slaughter@cs.stanford.edu>
2016-04-27 22:14:59 -0700

    Shrink delta time.

--- testcases/taylor_green_vortex/taylor_green_vortex_512_256_256.lua
+++ testcases/taylor_green_vortex/taylor_green_vortex_512_256_256.lua
@@ -9,7 +9,7 @@ return {
   -- if you increase the cell number and the calculation diverges
   -- right away, decrease the time step on the next line
   
-  delta_time = 2.0e-3,
+  delta_time = 1.0e-4,
 
   -- Control max iterations here. Set to a very high number if
   -- you want to run the full calculation (it will stop once
