2befe917dc2827d491e51b3c998e533b6b986c1d
Manolis Papadakis <manopapad@gmail.com>
2017-11-20 16:10:57 -0800

    Intensity output comparison script

---
+++ src/dom/compare.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+import argparse
+import itertools
+
+parser = argparse.ArgumentParser()
+parser.add_argument('f1')
+parser.add_argument('f2')
+args = parser.parse_args()
+
+maxDiff = 0.0
+with open(args.f1) as f1:
+    with open(args.f2) as f2:
+        for l1,l2 in itertools.izip(f1, f2):
+            if len(l1) > 1:
+                x1 = float(l1[:-1])
+                x2 = float(l2[:-1])
+                diff = abs((x1 - x2) / x1)
+                maxDiff = max(maxDiff, diff)
+print maxDiff
