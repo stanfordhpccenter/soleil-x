480677560f951110ec77b767d45b42397a9fc50f
Manolis Papadakis <manopapad@gmail.com>
2017-11-26 18:54:40 -0800

    Update script for new calling convention

--- src/run.sh
+++ src/run.sh
@@ -4,4 +4,4 @@ SOLEIL_SRC="$(cd "$(dirname "$(perl -MCwd -le 'print Cwd::abs_path(shift)' "${BA
 
 cd "$SOLEIL_SRC"
 
-LD_LIBRARY_PATH=.:"$LEGION_PATH"/bindings/terra/ ./soleil.exec ../testcases/cavity/cavity_32x32.json
+LD_LIBRARY_PATH=.:"$LEGION_PATH"/bindings/terra/ ./soleil.exec -i ../testcases/cavity/cavity_32x32.json
