a7fa35f9bf4b0583be320ae5fae572c2ca4ac9ad
Manolis Papadakis <manopapad@gmail.com>
2017-11-26 20:03:12 -0800

    Normalization script is now parametric on hdf library location

--- src/compile.sh
+++ src/compile.sh
@@ -2,10 +2,12 @@
 
 SOLEIL_SRC="$(cd "$(dirname "$(perl -MCwd -le 'print Cwd::abs_path(shift)' "${BASH_SOURCE[0]}")")" && pwd)"
 
+export HDF_LIBNAME=hdf5_serial
+export HDF_HEADER=hdf5/serial/hdf5.h
+
 cd "$SOLEIL_SRC"
 
-USE_HDF=1 HDF_LIBNAME=hdf5_serial HDF_HEADER=hdf5/serial/hdf5.h \
-    DEBUG=1 OBJNAME=soleil.exec \
+USE_HDF=1 DEBUG=1 OBJNAME=soleil.exec \
     "$LISZT_PATH"/liszt-legion.sh soleil-x.t \
     -i ../testcases/cavity/cavity_32x32.json \
     1> soleil.out 2> soleil.err
