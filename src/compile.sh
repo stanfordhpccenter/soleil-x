3634455e37e8ee3fec5a77bb541d9240e0b1da09
Manolis Papadakis <manopapad@gmail.com>
2017-11-26 17:14:40 -0800

    Script fixes

--- src/compile.sh
+++ src/compile.sh
@@ -13,6 +13,4 @@ USE_HDF=1 HDF_LIBNAME=hdf5_serial HDF_HEADER=hdf5/serial/hdf5.h \
        DEBUG=1 OBJNAME=soleil.exec \
        $LISZT_PATH/liszt-legion.sh soleil-x.t \
        -i ../testcases/cavity/cavity_32x32.json \
-       -fdebug 1 1> soleil.out 2> soleil.err
-
-cd -
+       -fflow 0 -fdebug 1 1> soleil.out 2> soleil.err
