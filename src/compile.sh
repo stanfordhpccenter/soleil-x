b5405c0ddbd5514013c7ef42ca7061c555bacfe1
Manolis Papadakis <manopapad@gmail.com>
2018-01-11 15:37:13 -0800

    Script simplifications

--- src/compile.sh
+++ src/compile.sh
@@ -11,7 +11,7 @@ export OBJNAME=soleil.exec
 
 # Regent options
 export TERRA_PATH=liszt/?.t
-export LIBRARY_PATH="${LIBRARY_PATH:-}:."
+export LIBRARY_PATH="."
 export INCLUDE_PATH="."
 
 gcc -g -O2 -c -o json.o json.c
