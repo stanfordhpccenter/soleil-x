50304bfec1a2ebbdd142bceb7681520d13c7abc3
Manolis Papadakis <manopapad@gmail.com>
2018-04-27 18:12:15 -0700

    Handle dynamic library path properly on Macs

--- src/dom_host.sh
+++ src/dom_host.sh
@@ -1,9 +1,15 @@
 #!/bin/bash -eu
 
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-}:$LEGION_DIR/bindings/regent/"
-export DYLD_LIBRARY_PATH="${LD_LIBRARY_PATH:-}:$LEGION_DIR/bindings/regent/"
-if [[ ! -z "${HDF_ROOT:-}" ]]; then
-    export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-}:$HDF_ROOT/lib"
+if [ "$(uname)" == "Darwin" ]; then
+    export DYLD_LIBRARY_PATH="${DYLD_LIBRARY_PATH:-}:$LEGION_DIR/bindings/regent/"
+    if [[ ! -z "${HDF_ROOT:-}" ]]; then
+        export DYLD_LIBRARY_PATH="${DYLD_LIBRARY_PATH:-}:$HDF_ROOT/lib"
+    fi
+else
+    export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-}:$LEGION_DIR/bindings/regent/"
+    if [[ ! -z "${HDF_ROOT:-}" ]]; then
+        export LD_LIBRARY_PATH="${LD_LIBRARY_PATH:-}:$HDF_ROOT/lib"
+    fi
 fi
 
 "$SOLEIL_DIR"/src/dom_host.exec "$@" -ll:csize 9000
