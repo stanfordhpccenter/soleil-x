c867678e82baf44a8f6fa1cb987e1bd4c3cb4612
Manolis Papadakis <manopapad@gmail.com>
2018-06-28 09:40:59 -0700

    Update some scripts

---
+++ scripts/add_density.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import argparse
+import h5py
+
+parser = argparse.ArgumentParser()
+parser.add_argument('hdf_file')
+parser.add_argument('density', type=float)
+args = parser.parse_args()
+
+f = h5py.File(args.hdf_file)
+f['density'] = len(f['temperature']) * [args.density]
+f.close()
