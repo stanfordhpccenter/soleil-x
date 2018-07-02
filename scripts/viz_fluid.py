31465c9c8869018fdf81e5ade671713b623fc819
Manolis Papadakis <manopapad@gmail.com>
2018-07-02 16:59:17 -0700

    Support visualization of multiple timesteps in the same file

--- scripts/viz_fluid.py
+++ scripts/viz_fluid.py
@@ -2,67 +2,89 @@
 
 import argparse
 import h5py
+import itertools
 import json
 
-XMF_TEMPLATE = """<?xml version="1.0" ?>
+XMF_HEADER = """<?xml version="1.0" ?>
 <!DOCTYPE Xdmf SYSTEM "Xdmf.dtd" []>
 <Xdmf xmlns:xi="http://www.w3.org/2003/XInclude" Version="3.0">
-  <Domain>
-    <Grid Name="Fluid" GridType="Uniform">
-      <!-- Topology: orthonormal 3D grid -->
-      <Topology TopologyType="3DCoRectMesh" Dimensions="GRID_DIMS"></Topology>
-      <!-- Geometry: Node positions derived implicitly, based on grid origin and cell size -->
-      <Geometry GeometryType="Origin_DxDyDz">
-        <DataItem Name="Origin" Dimensions="3" NumberType="Float" Precision="8" Format="XML">GRID_ORIGIN</DataItem>
-        <DataItem Name="Spacing" Dimensions="3" NumberType="Float" Precision="8" Format="XML">GRID_SPACING</DataItem>
-      </Geometry>
-      <Attribute Name="pressure" AttributeType="Scalar" Center="Cell">
-        <DataItem Dimensions="GRID_DIMS" NumberType="Float" Precision="8" Format="HDF">out.hdf:/pressure</DataItem>
-      </Attribute>
-      <Attribute Name="rho" AttributeTypen="Scalar" Center="Cell">
-        <DataItem Dimensions="GRID_DIMS" NumberType="Float" Precision="8" Format="HDF">out.hdf:/rho</DataItem>
-      </Attribute>
-      <Attribute Name="temperature" AttributeType="Scalar" Center="Cell">
-        <DataItem Dimensions="GRID_DIMS" NumberType="Float" Precision="8" Format="HDF">out.hdf:/temperature</DataItem>
-      </Attribute>
-      <Attribute Name="velocity" AttributeType="Vector" Center="Cell">
-        <DataItem Dimensions="GRID_DIMS 3" NumberType="Float" Precision="8" Format="HDF">out.hdf:/velocity</DataItem>
-      </Attribute>
+  <Domain Name="Fluid">
+    <Grid Name="FluidTimeSeries" GridType="Collection" CollectionType="Temporal">
+"""
+
+XMF_BODY = """
+      <Grid Name="Fluid" GridType="Uniform">
+        <Time Value="@TIMESTEP"/>
+        <!-- Topology: orthonormal 3D grid -->
+        <Topology TopologyType="3DCoRectMesh" Dimensions="@GRID_DIMS"></Topology>
+        <!-- Geometry: Node positions derived implicitly, based on grid origin and cell size -->
+        <Geometry GeometryType="Origin_DxDyDz">
+          <DataItem Name="Origin" Dimensions="3" NumberType="Float" Precision="8" Format="XML">@GRID_ORIGIN</DataItem>
+          <DataItem Name="Spacing" Dimensions="3" NumberType="Float" Precision="8" Format="XML">@GRID_SPACING</DataItem>
+        </Geometry>
+        <Attribute Name="pressure" AttributeType="Scalar" Center="Cell">
+          <DataItem Dimensions="@GRID_DIMS" NumberType="Float" Precision="8" Format="HDF">@HDF_FILE:/pressure</DataItem>
+        </Attribute>
+        <Attribute Name="rho" AttributeTypen="Scalar" Center="Cell">
+          <DataItem Dimensions="@GRID_DIMS" NumberType="Float" Precision="8" Format="HDF">@HDF_FILE:/rho</DataItem>
+        </Attribute>
+        <Attribute Name="temperature" AttributeType="Scalar" Center="Cell">
+          <DataItem Dimensions="@GRID_DIMS" NumberType="Float" Precision="8" Format="HDF">@HDF_FILE:/temperature</DataItem>
+        </Attribute>
+        <Attribute Name="velocity" AttributeType="Vector" Center="Cell">
+          <DataItem Dimensions="@GRID_DIMS 3" NumberType="Float" Precision="8" Format="HDF">@HDF_FILE:/velocity</DataItem>
+        </Attribute>
+      </Grid>
+"""
+
+XMF_FOOTER = """
     </Grid>
   </Domain>
 </Xdmf>
 """
 
 parser = argparse.ArgumentParser()
-parser.add_argument('hdf_file', help='simulation results to visualize')
-parser.add_argument('json_file', help='simulation config file')
-parser.add_argument('section_num', nargs='?', choices=['1','2'],
+parser.add_argument('json_file',
+                    help='original simulation configuration file')
+parser.add_argument('-s', '--section', choices=['1','2'],
                     help='which section to visualize (if multi-section sim)')
+parser.add_argument('hdf_file', nargs='+',
+                    help='fluid restart file(s) to visualize')
 args = parser.parse_args()
 
-hdf_in = h5py.File(args.hdf_file, 'r')
-# Extract domain size.
-nx = hdf_in['pressure'].shape[0]
-ny = hdf_in['pressure'].shape[1]
-nz = hdf_in['pressure'].shape[2]
-hdf_out = h5py.File('out.hdf', 'w')
-# Copy pressure over.
-hdf_out['pressure'] = hdf_in['pressure'][:]
-# Copy rho over.
-hdf_out['rho'] = hdf_in['rho'][:]
-# Copy temperature over.
-hdf_out['temperature'] = hdf_in['temperature'][:]
-# Convert velocity from an XxYxZ matrix of triples to an XxYxZx3 matrix.
-hdf_out['velocity'] = hdf_in['velocity'][:][:,:,:,:]
-hdf_out.close()
-hdf_in.close()
+nx = None
+ny = None
+nz = None
+
+for (f, i) in zip(args.hdf_file, itertools.count()):
+    hdf_in = h5py.File(f, 'r')
+    # Extract domain size.
+    if nx is None:
+        nx = hdf_in['pressure'].shape[0]
+        ny = hdf_in['pressure'].shape[1]
+        nz = hdf_in['pressure'].shape[2]
+    else:
+        assert nx == hdf_in['pressure'].shape[0]
+        assert ny == hdf_in['pressure'].shape[1]
+        assert nz == hdf_in['pressure'].shape[2]
+    hdf_out = h5py.File('out%s.hdf' % i, 'w')
+    # Copy pressure over.
+    hdf_out['pressure'] = hdf_in['pressure'][:]
+    # Copy rho over.
+    hdf_out['rho'] = hdf_in['rho'][:]
+    # Copy temperature over.
+    hdf_out['temperature'] = hdf_in['temperature'][:]
+    # Convert velocity from an XxYxZ matrix of triples to an XxYxZx3 matrix.
+    hdf_out['velocity'] = hdf_in['velocity'][:][:,:,:,:]
+    hdf_out.close()
+    hdf_in.close()
 
 # NOTE: We flip the X and Z dimensions, because Legion dumps data in
 # column-major order.
 with open(args.json_file) as json_in:
     config = json.load(json_in)
-    if args.section_num is not None:
-        config = config['configs'][int(args.section_num)-1]
+    if args.section is not None:
+        config = config['configs'][int(args.section)-1]
     # Compute number of boundary cells on each dimension.
     bx = nx - config['Grid']['zNum']
     by = ny - config['Grid']['yNum']
@@ -82,7 +104,12 @@ with open(args.json_file) as json_in:
 # NOTE: The XMF format expects grid dimensions in points, not cells, so we have
 # to add 1 on each dimension.
 with open('out.xmf', 'w') as xmf_out:
-    xmf_out.write(XMF_TEMPLATE
-                  .replace('GRID_DIMS', '%s %s %s' % (nx+1,ny+1,nz+1))
-                  .replace('GRID_ORIGIN', '%s %s %s' % (ox,oy,oz))
-                  .replace('GRID_SPACING', '%s %s %s' % (dx,dy,dz)))
+    xmf_out.write(XMF_HEADER)
+    for i in range(len(args.hdf_file)):
+        xmf_out.write(XMF_BODY
+                      .replace('@TIMESTEP', str(i))
+                      .replace('@GRID_DIMS', '%s %s %s' % (nx+1,ny+1,nz+1))
+                      .replace('@GRID_ORIGIN', '%s %s %s' % (ox,oy,oz))
+                      .replace('@GRID_SPACING', '%s %s %s' % (dx,dy,dz))
+                      .replace('@HDF_FILE', 'out%s.hdf' % i))
+    xmf_out.write(XMF_FOOTER)
