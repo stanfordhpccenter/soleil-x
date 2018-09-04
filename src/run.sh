f006e277c7e544e93d6beede10ac4022d2c3e013
Manolis Papadakis <manopapad@gmail.com>
2018-09-04 10:57:34 -0700

    Switch back to 1 utility processor

--- src/run.sh
+++ src/run.sh
@@ -120,7 +120,7 @@ function run_sapling {
         -x LD_LIBRARY_PATH -x SOLEIL_DIR -x REALM_BACKTRACE \
         "$EXECUTABLE" $ARGS \
         -ll:cpu 0 -ll:ocpu 1 -ll:onuma 0 -ll:okindhack -ll:othr 8 \
-        $GPU_OPTS -ll:util 2 -ll:dma 2 -ll:ahandlers 2 \
+        $GPU_OPTS -ll:dma 2 -ll:ahandlers 2 \
         -ll:csize 36000 -ll:ostack 8 -lg:sched -1
     # Resources:
     # 40230MB RAM per node
