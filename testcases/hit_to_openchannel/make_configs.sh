68336b83ecd06982e4c9d09a2da8c29667ad8402
Manolis Papadakis <manopapad@gmail.com>
2018-11-05 16:54:15 -0800

    Scripts to derive configs from dimensionless parameters

---
+++ testcases/hit_to_openchannel/make_configs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -eu
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+if [ $# -ne 2 ]; then
+    echo "Usage: $(basename "${BASH_SOURCE[0]}") <dat_file> <json_template>"
+    exit 1
+fi
+DAT_FILE="$1"
+JSON_TEMPLATE="$2"
+
+I=0
+while read -r LINE; do
+    "$SCRIPT_DIR"/make_config.py $LINE --json_template "$JSON_TEMPLATE" > run"$I".json
+    I="$(( I + 1 ))"
+done < "$DAT_FILE"
