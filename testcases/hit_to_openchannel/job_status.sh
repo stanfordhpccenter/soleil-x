6129b8b263cd2fbfb5751808a9f1b4563a2291e0
Manolis Papadakis <manopapad@gmail.com>
2019-02-24 15:06:07 -0800

    Update script

--- testcases/hit_to_openchannel/job_status.sh
+++ testcases/hit_to_openchannel/job_status.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -eu
 
+VERBOSE="${VERBOSE:-0}"
+
 for JOBOUT in "$@"; do
     JOBID="${JOBOUT%.out}"
     if [[ ! -e "$JOBOUT" ]]; then
@@ -20,17 +22,27 @@ for JOBOUT in "$@"; do
         echo "$JOBID: code 255"
     elif grep -q 'JSM server is not responding' "$JOBOUT"; then
         echo "$JOBID: jsm server not responding"
+    elif grep -q 'id.is_event' "$JOBOUT"; then
+        echo "$JOBID: realm assert: id.is_event"
     elif grep -q 'Ran out of space while copying particles from other section' "$JOBOUT"; then
         echo "$JOBID: channel section overflow"
     elif grep -q Successfully "$JOBOUT"; then
         echo -n "$JOBID: done"
-	FAILURES=`tail -q -n 1 "$SCRATCH"/"$JOBID"/sample*/console.txt | grep 'nan' | wc -l`
-	echo " ($FAILURES/32 cases diverged)"
+	if [[ "$VERBOSE" == 1 ]]; then
+  	    FAILURES=`tail -q -n 1 "$SCRATCH"/"$JOBID"/sample*/console.txt | grep 'nan' | wc -l`
+	    echo " ($FAILURES/32 cases diverged)"
+	else
+	    echo
+	fi
     elif grep -q summary "$JOBOUT"; then
         echo "$JOBID: OTHER ERROR"
     else
         echo -n "$JOBID: running"
-	FAILURES=`tail -q -n 1 "$SCRATCH"/"$JOBID"/sample*/console.txt | grep 'nan' | wc -l`
-	echo " ($FAILURES/32 cases diverged)"
+	if [[ "$VERBOSE" == 1 ]]; then
+	    FAILURES=`tail -q -n 1 "$SCRATCH"/"$JOBID"/sample*/console.txt | grep 'nan' | wc -l`
+	    echo " ($FAILURES/32 cases diverged)"
+	else
+	    echo
+	fi
     fi
 done
