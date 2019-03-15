8e47d390646066a462055bb0dd63ac44370adc38
Manolis Papadakis <manopapad@gmail.com>
2019-03-14 17:12:13 -0700

    Fix set of propagated env vars

--- src/run.sh
+++ src/run.sh
@@ -161,7 +161,7 @@ function run_sapling {
     source "$SOLEIL_DIR"/src/jobscript_shared.sh
     # Emit final command
     mpiexec -H "$NODES" --bind-to none \
-        -x LD_LIBRARY_PATH -x SOLEIL_DIR -x GASNET_BACKTRACE -x LEGION_FREEZE_ON_ERROR \
+        -x LD_LIBRARY_PATH -x SOLEIL_DIR -x REALM_BACKTRACE -x LEGION_FREEZE_ON_ERROR \
         $COMMAND
     # Resources:
     # 40230MB RAM per node
