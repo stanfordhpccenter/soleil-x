c30201a5cf005dbfc0a1b3fcc467ad2515fa44af
Manolis Papadakis <manopapad@gmail.com>
2019-07-10 14:32:34 -0700

    Improved timing script output

--- testcases/hit_to_openchannel/time_runs.py
+++ testcases/hit_to_openchannel/time_runs.py
@@ -9,16 +9,22 @@ import sys
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-t', '--target-iters', type=int, default=5000)
+parser.add_argument('-v', '--verbose', action='store_true')
 parser.add_argument('jobids', nargs='+')
 args = parser.parse_args()
 
 tmin_all = []
 tmax_all = []
 for j in args.jobids:
-    sys.stdout.write('%s: ' % j)
-    sys.stdout.flush()
+    if args.verbose:
+        sys.stdout.write('%s: ' % j)
+        sys.stdout.flush()
     status = subprocess.check_output([os.path.join(sys.path[0], 'job_status.sh'), j + '.out'])
-    assert status == 'done\n'
+    if status != 'done\n':
+        if args.verbose:
+            sys.stdout.write(status)
+            sys.stdout.flush()
+        continue
     tmin = float('inf')
     tmax = 0.0
     for console in glob.glob('%s/%s/sample*/console.txt' % (os.environ['SCRATCH'], j)):
@@ -31,5 +37,6 @@ for j in args.jobids:
         tmax = max(t, tmax)
     tmin_all.append(tmin)
     tmax_all.append(tmax)
-    print '%.3f %.3f' % (tmin, tmax)
-print 'Runs: %d, Average: %.3f, StdDev: %.2f%%' % (len(args.jobids), np.mean(tmax_all), 100.0*np.std(tmax_all, ddof=1)/np.mean(tmax_all))
+    if args.verbose:
+        print 'Min: %.3f, Max: %.3f' % (tmin, tmax)
+print 'Runs: %d, MinAvg: %.3f ~ %.2f%%, MaxAvg: %.3f ~ %.2f%%' % (len(tmax_all), np.mean(tmin_all), 100.0*np.std(tmin_all,ddof=1)/np.mean(tmin_all), np.mean(tmax_all), 100.0*np.std(tmax_all,ddof=1)/np.mean(tmax_all))
