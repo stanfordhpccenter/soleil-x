4ca9ff4e07b3463e485b731335fffeed515d4584
Alan Heirich <aheirich@stanford.edu>
2019-07-16 01:48:13 +0200

    scripts

--- scripts/make_job_chain_turbulence.bash
+++ scripts/make_job_chain_turbulence.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p $SCRATCH/job_chain
 rm -rf $SCRATCH/job_chain/*
-cd $SOLEIL_DIR/testcases/hit_to_openchannel && ./chain_jobs.py $SCRATCH/job_chain use_this_for_turbulence_visualization.json 1500 > __job_commands.txt
+cd $SOLEIL_DIR/testcases/hit_to_openchannel && ./chain_jobs.py $SCRATCH/job_chain viz_turbulence.json 1540 | sed -e "s/NODE=4/NODE=1/" > __job_commands.txt
--- scripts/make_turbulence_json.bash
+++ scripts/make_turbulence_json.bash
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd $PSAAP/soleil-x/testcases/hit_to_openchannel && ./make_cases.sh uncertainties_32.dat hf-titan.json 
+cd $SOLEIL_DIR/testcases/hit_to_openchannel && ./make_cases.sh uncertainties_32.dat hf-titan.json 
