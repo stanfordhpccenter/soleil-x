f56f283060425d07175d8bb59ee53ab1a633792a
Alan Heirich <aheirich@stanford.edu>
2019-08-06 00:37:21 +0200

    scripting run 24 hours

--- scripts/make_job_chain_turbulence.bash
+++ scripts/make_job_chain_turbulence.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 mkdir -p $SCRATCH/job_chain
 rm -rf $SCRATCH/job_chain/*
-cd $SOLEIL_DIR/testcases/hit_to_openchannel && ./chain_jobs.py $SCRATCH/job_chain viz_turbulence.json 7000 | sed -e "s/NODE=4/NODE=1/" > __job_commands.txt
+cd $SOLEIL_DIR/testcases/hit_to_openchannel && ./chain_jobs.py $SCRATCH/job_chain viz_turbulence.json 13860 | sed -e "s/NODE=4/NODE=1/" > __job_commands.txt
