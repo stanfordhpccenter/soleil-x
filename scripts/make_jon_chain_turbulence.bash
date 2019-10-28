401669bd5b3ba496fd93bf4d2e8453a2aa1b466b
Alan Heirich <aheirich@stanford.edu>
2019-10-28 21:22:24 +0100

    run scripts for piz daint turbulence demo

---
+++ scripts/make_jon_chain_turbulence.bash
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir -p $SCRATCH/job_chain
+rm -rf $SCRATCH/job_chain/*
+cd $SOLEIL_DIR/testcases/hit_to_openchannel && ./chain_jobs.py $SCRATCH/job_chain viz_turbulence.json 13860 | sed -e "s/NODE=4/NODE=1/" > __job_commands.txt
+
