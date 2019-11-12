177c06300789f3322423bd03b5db03ee64bd9486
Alan Heirich <aheirich@stanford.edu>
2019-11-12 20:33:36 +0100

    turbulence model

--- scripts/make_turbulence_json.bash
+++ scripts/make_turbulence_json.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
-module load cray-python/3.6.5.3
+module load cray-python/3.6.5.6
 cd $SOLEIL_DIR/testcases/hit_to_openchannel && ./make_cases.sh uncertainties_32.dat hf-pizdaint-24hrs.json --stagger=140
 
